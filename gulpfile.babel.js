30c8914c56a8f735a909aef8f8571f2b64ee0540
Kirill <chiril.ghermec@gmail.com>
2021-06-18 21:08:30 +0300

    Fixed Bugs with ICONS (not working)

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -61,13 +61,15 @@ const requireDir = require("require-dir"),
 
 requireDir("./gulp-tasks/");
 
-export { paths };
+export {
+    paths
+};
 
 export const development = gulp.series("clean", "smart-grid",
-    gulp.parallel(["views", "styles", "scripts", "images", "webp", "sprites", "fonts", "favicons"]),
+    gulp.parallel(["views", "styles", "scripts", "images", "webp", "sprites", "fonts"]),
     gulp.parallel("serve"));
 
 export const prod = gulp.series("clean",
-    gulp.parallel(["views", "styles", "scripts", "images", "webp", "sprites", "fonts", "favicons", "gzip"]));
+    gulp.parallel(["views", "styles", "scripts", "images", "webp", "sprites", "fonts", "gzip"]));
 
 export default development;
